3f13577c40a8d4b450a0fa2ea74faec0f279847f
Jayesh baviskar <jayesh.intheworld1987@gmail.com>
2020-11-04 19:40:54 +0530

    Add files via upload

---
+++ Scrap/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scrap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<String> mainPageLinks = Scrapper.GetMainPageLinks("https://newyork.craigslist.org/d/computer-gigs/search/cpg");
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < mainPageLinks.Count; i++)
+            {
+                builder.Append(mainPageLinks[i] +"\n");
+            }
+
+            richTextBox1.Text = builder.ToString();
+        }
+    }
+}
